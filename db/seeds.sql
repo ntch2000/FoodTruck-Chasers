2cbf8f11975bb0fa312046b311cb0667e7c3b3c7
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-12-12 11:29:53 -0500

    Added seeds.sql file for local testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,4 @@
--- creates initial database
-DROP DATABASE IF EXISTS food_trucks_db;
-CREATE DATABASE food_trucks_db;
-
--- sample mysql code for testing locally
-USE food_trucks_db;
-
-CREATE TABLE food_trucks(
-    id INT NOT NULL AUTO_INCREMENT,
-    truck_name VARCHAR(100) NOT NULL,
-    operator_firstName VARCHAR(100) NOT NULL,
-    operator_lastName VARCHAR(100) NOT NULL,
-    phone VARCHAR(100) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    category VARCHAR(100) NOT NULL,
-    promo_message TEXT,
-    PRIMARY KEY(id)
-);
+-- Initial seeds information
 
 INSERT INTO food_trucks (truck_name, operator_firstName, operator_lastName, phone, city, category)
 VALUES ("Truck 1", "Neil", "Gandhi", "1234567890", "Charleston", "Mexican");
