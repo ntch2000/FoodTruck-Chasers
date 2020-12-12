baa7fd900abe471b1f588ddbbc79044bc767114c
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-12-12 10:46:47 -0500

    Created db folder with initial schema.sql file. Edited typo errors in food_trucks.js file

--- models/food_trucks.js
+++ models/food_trucks.js
@@ -1,15 +1,14 @@
 module.exports = function (sequelize, DataTypes) {
-    const Game = sequelize.define("food_truck", {
-      tuck_name: DataTypes.STRING,
-      operator_firstname: DataTypes.STRING,
-      operator_LastName: DataTypes.STRING,
-      phone: DataTypes.INTEGER,
-      city: DataTypes.STRING,
-      category: DataTypes.STRING,
-      promo_message: DataTypes.STRING,
-    
-    });
+  const Game = sequelize.define("food_truck", {
+    truck_name: DataTypes.STRING,
+    operator_firstName: DataTypes.STRING,
+    operator_LastName: DataTypes.STRING,
+    // maybe change the datatype for phone number
+    phone: DataTypes.INTEGER,
+    city: DataTypes.STRING,
+    category: DataTypes.STRING,
+    promo_message: DataTypes.STRING,
+  });
 
-  
-    return food_truck;
-  };
+  return food_truck;
+};
