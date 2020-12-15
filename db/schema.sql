1cf3dd2375b574f74658324f002bb415e0113e1b
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-12-14 20:13:44 -0500

    modified model to have operator_username rather than full name

--- db/schema.sql
+++ db/schema.sql
@@ -8,8 +8,7 @@ USE food_trucks_db;
 CREATE TABLE food_trucks(
     id INT NOT NULL AUTO_INCREMENT,
     truck_name VARCHAR(100) NOT NULL,
-    operator_firstName VARCHAR(100) NOT NULL,
-    operator_lastName VARCHAR(100) NOT NULL,
+    operator_username VARCHAR(100) NOT NULL,
     phone VARCHAR(100) NOT NULL,
     city VARCHAR(100) NOT NULL,
     category VARCHAR(100) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 -- Initial seeds information
 
-INSERT INTO food_trucks (truck_name, operator_firstName, operator_lastName, phone, city, category)
-VALUES ("Truck 1", "Neil", "Gandhi", "1234567890", "Charleston", "Mexican");
+INSERT INTO food_trucks (truck_name, operator_username, phone, city, category)
+VALUES ("Truck 1", "Neil", "1234567890", "Charleston", "Mexican");
 
 INSERT INTO food_trucks (truck_name, operator_firstName, operator_lastName, phone, city, category, promo_message)
-VALUES ("Truck 2", "Brian", "Shanken", "9876543210", "Atlanta", "Fusion", "This is a promo message");
+VALUES ("Truck 2", "Brian", "9876543210", "Atlanta", "Fusion", "This is a promo message");
 
 INSERT INTO food_trucks (truck_name, operator_firstName, operator_lastName, phone, city, category)
-VALUES ("Truck 3", "Scott", "Wells", "1239874562", "Atlanta", "Italian");
+VALUES ("Truck 3", "Scott", "1239874562", "Atlanta", "Italian");
 
 INSERT INTO food_trucks (truck_name, operator_firstName, operator_lastName, phone, city, category, promo_message)
-VALUES ("Truck 4", "Dennis", "Francis", "6541239637", "Atlanta", "Korean", "Buy 2 GET 1 Free!!");
+VALUES ("Truck 4", "Dennis", "6541239637", "Atlanta", "Korean", "Buy 2 GET 1 Free!!");
